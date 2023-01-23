acb9ee6e0d08722e0be83eaefa138a0892bc900f
Polina <ms.maylin@inbox.ru>
2023-01-23 17:03:40 +0300

    In task3: another solution added in program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,15 +3,36 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-System.Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 0)
-    for (int i = 1; i <= number; i++)
+// if (number > 0)
+//     for (int i = 1; i <= number; i++)
+//     {
+//         System.Console.Write($"{Math.Pow(i, 3)} ");
+//     }
+// else
+// {
+//     System.Console.WriteLine("Введённое число меньше 1.");
+// }
+
+// Второй способ
+
+System.Console.WriteLine("Введите число: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+if (size > 0)
+{
+    int[] array = new int[size];
+    int index = 0;
+    while (index < size)
     {
-        System.Console.Write($"{Math.Pow(i, 3)} ");
+        array[index] = index + 1;
+        System.Console.Write($"{Math.Pow(array[index], 3)} ");
+        index++;
     }
+}
 else
 {
-    System.Console.WriteLine("Введённое число меньше 1.");
+    System.Console.WriteLine($"Введённое число меньше 1.");
 }
